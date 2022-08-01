ecf2d2c67b818be28e2d5893b86db8d2c0cd4d60
Jean Carlo Gómez Narváez <gomezjean266@gmail.com>
2022-07-31 21:30:07 -0500

    Modificando la logica de Router

--- Router.php
+++ Router.php
@@ -19,7 +19,11 @@ class Router
 
     public function comprobarRutas()
     {
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
